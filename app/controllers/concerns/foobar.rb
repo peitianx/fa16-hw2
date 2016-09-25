813f1ab8f0b7209ad4dee150fe154a688dafa78e
peitianx <peitian.xiong@berkeley.edu>
2016-09-25 15:37:02 -0700

    finish hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,10 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(param)
+  	@var = param
+  end
+
+  def bar(a, b)
+  	return "#{a}#{@var}#{b.values[0]}";
   end
 end
--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -3,26 +3,32 @@ class Person
 
   def initialize(name, age)
     @name = name
-    @age = age
+    @age = age.to_i
   end
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'nickname'
+      @name[0,4]
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    Date.current.year - @age
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'introduce'
+    return "My namse is #{name}. My age is #{age}."
   end
 
   def fib_number
     # YOUR IMPLMENTATION HERE
-    0
+    i = x = 0
+    y = 1
+    while i < @age do
+      x, y = y, x + y
+      i += 1
+    end
+    return x
   end
 end
